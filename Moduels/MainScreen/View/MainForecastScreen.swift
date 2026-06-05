77094106a5736c19fd1f4f2cf8a260d80e6db84b
magdy <mohamedmagdy800k@gmil.com>
2026-06-05 20:30:42 +0200

    Test The branch

---
+++ Moduels/MainScreen/View/MainForecastScreen.swift
@@ -0,0 +1,20 @@
+//
+//  MainForecastScreen.swift
+//  Ajwa
+//
+//  Created by Mohamed Magdy on 05/06/2026.
+//
+
+import SwiftUI
+
+struct MainForecastScreen: View {
+    var body: some View {
+        Text("Hello, World! \nFrom Main Forecase Screen")
+    }
+}
+
+struct MainForecastScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        MainForecastScreen()
+    }
+}
